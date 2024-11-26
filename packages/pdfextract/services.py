d2e62bc04c3919605ec6e733fa511136ce38c551
sigdelsanjog <sigdelsanjog@gmail.com>
2024-11-26 22:42:24 +0545

    added TODO comments for text preprocessing and image extraction in PDF processing

--- packages/pdfextract/services.py
+++ packages/pdfextract/services.py
@@ -3,6 +3,26 @@ from pathlib import Path
 from PIL import Image
 import io
 
+'''
+TODO: 
+    1. Preprocess the extracted text
+    1.a create a json file for the extracted text where key will be the paper title and value will be the content inside that paper
+    2. Or instead create a general template to map the key value pair within the extracted paper. Example below in 2.1
+    2.1 {Authors: [Author1, Author2, Author3], 
+        Abstract: "Abstract content",
+        Introduction: "Introduction content",
+        RElated Work: "Related work content",
+        Methodology: "Methodology content",
+        Results: "Results content",
+        Conclusion: "Conclusion content",
+        References: [Reference1, Reference2, Reference3]}
+
+    Extract images from the PDF file and save them to the "extracted_images" directory.
+    2. Append the file paths of the extracted images to the "images" list in the "result" dictionary.
+    3. Or store the downloaded images in a database and return the image URLs in the "images" list.
+    -- The images inside a research paper is not that important inLLMS yet. So we can actually skip this portaion at the moment.
+'''
+
 def extract_text_and_images(pdf_path: str):
     result = {"text": "", "images": []}
     reader = PdfReader(pdf_path)
